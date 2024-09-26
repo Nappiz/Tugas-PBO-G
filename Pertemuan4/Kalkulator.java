11b9c463b89675dbc038af1635c87f41f593f5db
Badruzzaman Nafiz <149480285+Nappiz@users.noreply.github.com>
2024-09-26 13:22:53 +0700

    Create Kalkulator.java

---
+++ Pertemuan4/Kalkulator.java
@@ -0,0 +1,37 @@
+/**
+ * Write a description of class TicketMachine here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class Kalkulator {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        Operasi operasi = new Operasi();
+
+        System.out.print("Angka pertama: ");
+        float number1 = scanner.nextFloat();
+        operasi.setNumber1(number1);
+
+        System.out.print("Angka kedua: ");
+        float number2 = scanner.nextFloat();
+        operasi.setNumber2(number2);
+
+        System.out.print("Operasi (1: Penjumlahan, 2: Pengurangan, 3: Perkalian, 4: Pembagian): ");
+        int pilihan = scanner.nextInt();
+
+        if(pilihan == 1){
+            System.out.println("Hasil: " + operasi.penjumlahan());
+        }else if (pilihan == 2){
+            System.out.println("Hasil: " + operasi.pengurangan());
+        }else if (pilihan == 3){
+            System.out.println("Hasil: " + operasi.perkalian());
+        }else if (pilihan == 4){
+            System.out.println("Hasil: " + operasi.pembagian());
+        }else{
+            System.out.println("Error!");
+        }
+    }
+}
