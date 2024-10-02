21210bd7c2fdb4b53affb60b1e92c26cb50adc3e
Badruzzaman Nafiz <149480285+Nappiz@users.noreply.github.com>
2024-10-02 19:25:38 +0700

    Create Person.java

---
+++ Pertemuan5/Person.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Person here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+}
