fb2cf1240051692ef7e948ce25e2acbeb217fa36
Badruzzaman Nafiz <149480285+Nappiz@users.noreply.github.com>
2024-10-25 01:53:39 +0700

    Create Cat.java

---
+++ ETS/1/Cat.java
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Cat extends Animal {
+    public void bark() {
+        System.out.println("Meow");
+    }
+}
+
