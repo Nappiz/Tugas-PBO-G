8215b19e2aaccad055d0debc4006f746e48bc458
Badruzzaman Nafiz <149480285+Nappiz@users.noreply.github.com>
2024-10-25 01:52:58 +0700

    Create Animal.java

---
+++ ETS/1/Animal.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Animal here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Animal {
+
+    public void makeSound() {
+        System.out.println("Animal is making a sound");
+    }
+}
+
