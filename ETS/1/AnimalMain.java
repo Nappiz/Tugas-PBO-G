7180241db445dbcb8447c71fd5c6b5b7cb57195e
Badruzzaman Nafiz <149480285+Nappiz@users.noreply.github.com>
2024-10-25 01:54:03 +0700

    Create AnimalMain.java

---
+++ ETS/1/AnimalMain.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class AnimalMain here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AnimalMain {
+    public static void main(String[] args) {
+        Cat MyCat = new Cat();
+        MyCat.makeSound();
+        MyCat.bark();
+    }
+}
