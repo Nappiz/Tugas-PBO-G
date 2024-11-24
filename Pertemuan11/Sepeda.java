d9eaafda7dc552cadc0102b7fe5aaf3ad2e6ee06
Badruzzaman Nafiz <149480285+Nappiz@users.noreply.github.com>
2024-11-24 22:28:11 +0700

    Create Sepeda.java

---
+++ Pertemuan11/Sepeda.java
@@ -0,0 +1,12 @@
+public class Sepeda extends Kendaraan {
+    private String jenisSepeda;
+
+    public Sepeda(String merk, String model, int tahunProduksi, String jenisSepeda) {
+        super(merk, model, tahunProduksi);
+        this.jenisSepeda = jenisSepeda;
+    }
+
+    public String getDetail() {
+        return "Sepeda: " + getMerk() + " " + getModel() + " (" + getTahunProduksi() + "), Jenis: Sepeda " + jenisSepeda;
+    }
+}
